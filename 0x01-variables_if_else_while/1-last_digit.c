571b48bd31f5c71ab3cbf089a33ffd9896eabd14
Ayoola Ayomide <mhydeh116@icloud.com>
2022-09-12 06:44:57 -0700

    For Me

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,41 @@
+#include <stdio.h>
+
 #include <stdlib.h>
 
 #include <time.h>
 
-/* more headers goes there */
-
-
-
-/* betty style doc for function main goes there */
+/**
+ *
+ * * main - Starts the program
+ *
+ * * Description: Betty coding style
+ *
+ * * Return: Always 0 (success)
+ *
+ * */
 
 int main(void)
 
 {
 
-		int n;
+	int n;
 
+	srand(time(0));
 
+	n = rand() - RAND_MAX / 2;
 
-			srand(time(0));
+	if (n > 0)
 
-				n = rand() - RAND_MAX / 2;
+		printf("%d is positive\n", n);
 
-					/* your code goes there */
+	else if (n == 0)
 
-					return (0);
+		printf("%d is zero\n", n);
+
+	else
+
+		printf("%d is negative\n", n);
+
+	return (0);
 
 }
