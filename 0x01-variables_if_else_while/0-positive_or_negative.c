ee6a7999fd151fd20865bd4a75f9ae604e350c04
Ayoola Ayomide <mhydeh116@icloud.com>
2022-09-12 13:55:16 -0700

    Done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,8 +7,7 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{
-	int n;
+{	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -17,6 +16,5 @@ int main(void)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-
 	return (0);
 }
