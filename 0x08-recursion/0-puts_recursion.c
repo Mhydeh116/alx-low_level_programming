aa3fe485ab0d955d33e5865799951fbdce304896
Ayoola Ayomide <mhydeh116@icloud.com>
2022-09-29 09:08:20 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
-
 /**
- * _print_rev_recursion - this function prints a string recursively in reverse
- * @s: This is the string
- * Return: Void
+ * _puts_recursion - this function prints a string to stdout recursively
+ * @s: This is the string to be printed
+ * Return: Nothing
  */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(++s);
 }
